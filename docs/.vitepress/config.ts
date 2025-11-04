ce8d5d9c0908d86e7ce87ea26ed5d4e880fb9665
susu <3211134947@qq.com>
2025-11-04 13:13:58 +0800

    修复路由大小写问题

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
         items: [
           { text: '按钮 Button', link: '/components/button'},
           { text: '布局容器 Container', link: '/components/container'},
-          { text: '图标 Icon', link: 'components/icon'},
+          { text: '图标 Icon', link: '/components/icon'},
           { text: '链接 Link', link: '/components/link'},
           { text: '折叠面板 Collapse', link: '/components/collapse'},
         ]
@@ -33,21 +33,21 @@ export default defineConfig({
         text: '反馈',
         items: [
           { text: '反馈 Alert', link: '/components/alert'},
-          { text: '消息提示 Message', link: '/components/Message'},
-          { text: '消息弹出框 MessageBox', link: '/components/MessageBox'},
-          { text: '文字提示 Tooltip', link: '/components/Tooltip'},
-          { text: '下拉菜单 Dropdown', link: '/components/DropDown'}
+          { text: '消息提示 Message', link: '/components/message'},
+          { text: '消息弹出框 MessageBox', link: '/components/messagebox'},
+          { text: '文字提示 Tooltip', link: '/components/tooltip'},
+          { text: '下拉菜单 Dropdown', link: '/components/dropdown'}
         ]
       },
       {
         text: '数据输入',
         items: [
-          { text: '开关 Switch', link: 'components/switch'},
-          { text: '评分 Rate', link: 'components/rate'},
-          { text: '输入框 Input', link: 'components/input'},
-          { text: '选择框 Select', link: 'components/select'},
+          { text: '开关 Switch', link: '/components/switch'},
+          { text: '评分 Rate', link: '/components/rate'},
+          { text: '输入框 Input', link: '/components/input'},
+          { text: '选择框 Select', link: '/components/select'},
           { text: '表单 Form', link: '/components/form'},          
-          { text: '日期选择器 DatePicker', link: 'components/datepicker'},
+          { text: '日期选择器 DatePicker', link: '/components/datepicker'},
         ]
       }
     ],
